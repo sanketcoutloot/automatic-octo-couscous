3ee2b043e3de5286e01d789d1f563f690a029087
sanketcoutloot <sanketcoutloot@gmail.com>
2020-12-21 19:38:50 +0530

    sorting sorted

--- src/component/ReactTable/ReactTable.js
+++ src/component/ReactTable/ReactTable.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useTable, useFilters, useGlobalFilter } from 'react-table';
+import {
+    useTable,
+    useFilters,
+    useGlobalFilter,
+    usePagination,
+    useSortBy,
+} from 'react-table';
 import {
     Input,
     InputGroup,
@@ -108,15 +114,31 @@ function ReactTable({ columns, data }) {
         visibleColumns,
         preGlobalFilteredRows,
         setGlobalFilter,
+        page, // Instead of using 'rows', we'll use page,
+        // which has only the rows for the active page
+
+        // The rest of these things are super handy, too ;)
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
     } = useTable(
         {
             columns,
             data,
+            initialState: { pageIndex: 2 },
         },
         useFilters, // useFilters!
-        useGlobalFilter // useGlobalFilter!
+        useGlobalFilter,
+        useSortBy, // useGlobalFilter!,
+        usePagination
     );
 
+    let { pageIndex, pageSize } = state;
     return (
         <Box>
             <Table {...getTableProps()}>
@@ -144,15 +166,26 @@ function ReactTable({ columns, data }) {
                     {headerGroups.map((headerGroup) => (
                         <TableHeader {...headerGroup.getHeaderGroupProps()}>
                             {headerGroup.headers.map((column) => (
-                                <th {...column.getHeaderProps()}>
+                                <th
+                                    {...column.getHeaderProps(
+                                        column.getSortByToggleProps()
+                                    )}
+                                >
                                     {column.render('Header')}
+                                    <span>
+                                        {column.isSorted
+                                            ? column.isSortedDesc
+                                                ? ' 🔽'
+                                                : ' 🔼'
+                                            : ''}
+                                    </span>
                                 </th>
                             ))}
                         </TableHeader>
                     ))}
                 </thead>{' '}
                 <TableBody {...getTableBodyProps()}>
-                    {rows.map((row, i) => {
+                    {page.map((row, i) => {
                         prepareRow(row);
                         return (
                             <TableRow {...row.getRowProps()}>
@@ -168,6 +201,58 @@ function ReactTable({ columns, data }) {
                     })}
                 </TableBody>
             </Table>
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button
+                    onClick={() => previousPage()}
+                    disabled={!canPreviousPage}
+                >
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button
+                    onClick={() => gotoPage(pageCount - 1)}
+                    disabled={!canNextPage}
+                >
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={(e) => {
+                            const page = e.target.value
+                                ? Number(e.target.value) - 1
+                                : 0;
+                            gotoPage(page);
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={(e) => {
+                        setPageSize(Number(e.target.value));
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </Box>
     );
 }
