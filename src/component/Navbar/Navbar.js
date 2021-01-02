f284e353a5ff6274211f7c57f05c7c042cdddadf
sanketcoutloot <sanketcoutloot@gmail.com>
2021-01-02 19:10:15 +0530

    fixed header icon size

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,310 +1,133 @@
 import {
-  Box,
-  ListItem,
-  UnorderedList,
-  Link,
-  Image,
-  Icon,
-  Flex,
-} from "@chakra-ui/react";
-import { NavLink as RouterLink } from "react-router-dom";
-import React from "react";
+    Box,
+    ListItem,
+    UnorderedList,
+    Link,
+    Image,
+    Icon,
+    Flex,
+} from '@chakra-ui/react';
+import { NavLink as RouterLink } from 'react-router-dom';
+import React from 'react';
 
-import { FaBars, FaGreaterThan, FaHome, FaUserAlt } from "react-icons/fa";
+import { FaBars, FaGreaterThan, FaHome, FaUserAlt } from 'react-icons/fa';
 
-import Logo from "../../asset/cl-logo.png";
+import Logo from '../../asset/cl-logo.png';
 const ActiveLinkStyle = {
-  fontWeight: "bold",
-  color: "blue",
+    fontWeight: 'bold',
+    color: 'blue',
 };
 
 const Navbar = () => (
-  <Box as="nav" bg="teal" position="relative">
-    <Box as="div" position="sticky" top="0" backgroundColor="white">
-      <Box display="flex" m={3} justifyContent="space-around">
-        <Icon as={FaBars} color="gray.500" h={6} w={6} />
-        <img
-          src={Logo}
-          style={{
-            height: "1.7rem",
-            width: "9rem",
-            marginLeft: "auto",
-            marginRight: "auto",
-          }}
-        />
-      </Box>
+    <Box as="nav" bg="teal" position="relative">
+        <Box as="div" position="sticky" top="0" backgroundColor="white">
+            <Box display="flex" m={3} justifyContent="space-around">
+                <Icon as={FaBars} color="gray.500" h={6} w={6} />
+                <img
+                    src={Logo}
+                    style={{
+                        height: '1.7rem',
+                        width: '9rem',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                    }}
+                />
+            </Box>
 
-      <Box
-        display="flex"
-        m={2}
-        alignItems="center"
-        justifyContent="space-around"
-      >
-        <Box borderRadius="md" border="grey 2px solid" bgColor="white" p={1}>
-          <Icon boxSize={7} as={FaUserAlt} w={6} h={6} />
+            <Box
+                display="flex"
+                m={2}
+                alignItems="center"
+                justifyContent="space-around"
+            >
+                <Box
+                    borderRadius="md"
+                    border="grey 2px solid"
+                    bgColor="white"
+                    p={1}
+                >
+                    <Icon as={FaUserAlt} w={6} h={6} />
+                </Box>
+                <Box textAlign="center">
+                    <Box> Mahima </Box>
+                    <Box fontSize="xs"> co-founder </Box>
+                </Box>
+            </Box>
         </Box>
-        <Box textAlign="center">
-          <Box> Mahima </Box>
-          <Box fontSize="xs"> co-founder </Box>
-        </Box>
-      </Box>
+
+        <hr style={{ margin: '1.1rem' }} />
+
+        <UnorderedList listStyleType="none" margin="none">
+            <ListItem
+                display="flex"
+                alignItems="center"
+                // justifyContent="space-between"
+                m={2}
+            >
+                <Icon boxSize={3} mr={6} as={FaHome} />
+                <Link
+                    as={RouterLink}
+                    to="/users"
+                    activeStyle={{ ...ActiveLinkStyle }}
+                >
+                    Users
+                </Link>
+                <Icon
+                    ml="auto"
+                    mr="2.5rem"
+                    boxSize={3}
+                    color="gray.500"
+                    as={FaGreaterThan}
+                />
+            </ListItem>
+
+            <ListItem
+                display="flex"
+                alignItems="center"
+                // justifyContent="space-between"
+                m={2}
+            >
+                <Icon boxSize={3} mr={6} as={FaHome} />
+                <Link
+                    as={RouterLink}
+                    to="/add/users"
+                    activeStyle={{ ...ActiveLinkStyle }}
+                >
+                    Users
+                </Link>
+                <Icon
+                    ml="auto"
+                    mr="2.5rem"
+                    boxSize={3}
+                    color="gray.500"
+                    as={FaGreaterThan}
+                />
+            </ListItem>
+
+            <ListItem
+                display="flex"
+                alignItems="center"
+                // justifyContent="space-between"
+                m={2}
+            >
+                <Icon mr={6} boxSize={3} as={FaHome} />
+                <Link
+                    as={RouterLink}
+                    to="/submitusers"
+                    activeStyle={{ ...ActiveLinkStyle }}
+                >
+                    sumbit Users
+                </Link>
+                <Icon
+                    ml="auto"
+                    mr="2.5rem"
+                    boxSize={3}
+                    color="gray.500"
+                    as={FaGreaterThan}
+                />
+            </ListItem>
+        </UnorderedList>
     </Box>
-
-    <hr style={{ margin: "1.1rem" }} />
-
-    <UnorderedList listStyleType="none" margin="none">
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link as={RouterLink} to="/users" activeStyle={{ ...ActiveLinkStyle }}>
-          Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/add/users"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/submitusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          sumbit Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-
-      <ListItem
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        m={2}
-      >
-        <Icon as={FaHome} />
-        <Link
-          as={RouterLink}
-          to="/testusers"
-          activeStyle={{ ...ActiveLinkStyle }}
-        >
-          test Users
-        </Link>
-        <Icon color="gray.500" as={FaGreaterThan} />
-      </ListItem>
-    </UnorderedList>
-  </Box>
 );
 
 export default Navbar;
