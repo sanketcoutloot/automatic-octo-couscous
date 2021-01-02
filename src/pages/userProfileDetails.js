d6624d37f8db62ef150ee0b9423fd416518162e3
sanketcoutloot <sanketcoutloot@gmail.com>
2021-01-02 17:30:25 +0530

    edit bank details

--- src/pages/userProfileDetails.js
+++ src/pages/userProfileDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useForm } from 'react-hook-form/dist/index.ie11';
 import {
     Box,
     Breadcrumb,
@@ -18,6 +19,20 @@ import {
     Grid,
     GridItem,
     Input,
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    NumberInput,
+    NumberInputField,
+    Select,
 } from '@chakra-ui/react';
 
 import BANK from '../asset/bank.svg';
@@ -29,6 +44,32 @@ const userProfileDetails = () => {
     const { state } = useLocation();
     const [userId, setUserId] = useState('');
     const [userName, setUserName] = useState('');
+
+    let requestedBankDetails = {
+        accountHolderName: 'Umesh Singh',
+        accountNumber: '00762121033878',
+        accountType: 'Saving Account',
+        bankName: 'Punjab National Bank',
+        ifscCode: 'PUNB0007610',
+    };
+    // modal control
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    //    fom controls
+    const { handleSubmit, errors, register, formState, getValues } = useForm({
+        defaultValues: {
+            ...requestedBankDetails,
+        },
+    });
+
+    function onSubmit() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                alert(JSON.stringify(getValues(), null, 2));
+                resolve();
+            }, 3000);
+        });
+    }
 
     useEffect(() => {
         setUserId(() => state.userId);
@@ -200,6 +241,7 @@ const userProfileDetails = () => {
                     <Flex justify="space-between" align="center">
                         <Text>Bank Details :</Text>
                         <Button
+                            onClick={onOpen}
                             colorScheme="blue"
                             leftIcon={<FaPen />}
                             variant="outline"
@@ -260,6 +302,133 @@ const userProfileDetails = () => {
                     </Grid>
                 </Box>
             </Flex>
+            {/* Edit modal code  */}
+            <Modal
+                isCentered
+                blockScrollOnMount={false}
+                isOpen={isOpen}
+                onClose={onClose}
+            >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Edit Bank Details </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            {/* Account Holder name  */}
+                            <FormControl isRequired isInvalid={errors.name}>
+                                <FormLabel htmlFor="accountHolderName">
+                                    Account Holder Name
+                                </FormLabel>
+                                <Input
+                                    name="accountHolderName"
+                                    placeholder="Account Holder Name "
+                                    ref={register({
+                                        required: true, // JS only: <p>error message</p> TS only support string
+                                    })}
+                                />
+                                <FormErrorMessage>
+                                    {errors.name && errors.name.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            {/* Account Number  */}
+                            <FormControl isRequired isInvalid={errors.name}>
+                                <FormLabel htmlFor="accountNumber">
+                                    Account Number
+                                </FormLabel>
+                                <NumberInput name="accountNumber">
+                                    <NumberInputField
+                                        placeholder="Account Number"
+                                        ref={register({
+                                            required: true,
+                                        })}
+                                    />
+                                </NumberInput>
+                                <FormErrorMessage>
+                                    {errors.name && errors.name.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            {/* Accoubt Type */}
+                            <FormControl isInvalid={errors.name}>
+                                <FormLabel htmlFor="accountType">
+                                    Account Type
+                                </FormLabel>
+                                <Select
+                                    name="accountType"
+                                    placeholder="Account Type"
+                                    ref={register({
+                                        required: true,
+                                    })}
+                                >
+                                    <option value="Saving Account">
+                                        Saving Account
+                                    </option>
+                                    <option value="Current Account">
+                                        Current Account
+                                    </option>
+                                    <option value="UPI">UPI</option>
+                                </Select>
+
+                                <FormErrorMessage>
+                                    {errors.name && errors.name.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            {/* Bank Name  */}
+                            <FormControl isRequired isInvalid={errors.name}>
+                                <FormLabel htmlFor="bankName">
+                                    Bank Name
+                                </FormLabel>
+                                <Input
+                                    name="bankName"
+                                    placeholder="Account Holder Name "
+                                    ref={register({
+                                        required: true,
+                                    })}
+                                />
+                                <FormErrorMessage>
+                                    {errors.name && errors.name.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            {/* IFSC CODE  */}
+                            <FormControl isRequired isInvalid={errors.name}>
+                                <FormLabel htmlFor="ifscCode">
+                                    IFSC code
+                                </FormLabel>
+                                <Input
+                                    name="ifscCode"
+                                    placeholder="Account Holder Name "
+                                    ref={register({
+                                        required: true,
+                                    })}
+                                />
+                                <FormErrorMessage>
+                                    {errors.name && errors.name.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            <Button
+                                mt={4}
+                                colorScheme="teal"
+                                isLoading={formState.isSubmitting}
+                                type="submit"
+                            >
+                                Submit
+                            </Button>
+                        </form>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant="ghost">Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Box>
     );
 };
